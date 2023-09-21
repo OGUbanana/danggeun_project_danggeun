30ebf91e6d5630b30ab57736a719d6eccb6edb53
LeeYeonghwan <129285372+Leeeyounghwan@users.noreply.github.com>
2023-09-21 16:46:31 +0900

    feat: 로그인 및 회원가입 기능 구현 (#9)
    * feat: 회원가입 및 로그인 폼 구현
    
    Resolves: #6
    
    * feat: 회원가입 및 로그인 기능 구현
    
    Resolves: #6

--- danggeun/market/views.py
+++ danggeun/market/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import logout, authenticate
+from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from .forms import CustomAuthForm, CustomUserForm
+from django.contrib.auth.models import User
 
 def main(request):
     return render(request, 'main.html')
@@ -14,30 +15,36 @@ def search(request):
 def chat(request):
     return render(request, 'chat.html')
 
-def login(request):
+def user_login(request):
     if request.user.is_authenticated:
-        return redirect('market:main')
+        return redirect('main')
     else:
         form = CustomAuthForm(data=request.POST or None)
         if request.method == "POST":
             if form.is_valid():
                 user_id = form.cleaned_data['username']
-                user_pwd = form.cleaned_data['userpassword_pwd']
+                user_pwd = form.cleaned_data['password']
                 user = authenticate(request, username=user_id, password=user_pwd)
                 if user is not None:
                     login(request, user)
                     return redirect('market:main')
         return render(request, 'registration/login.html', {"form": form})
 
-# def logout(request):
-#     return render(request, 'registration/logout.html')
+def user_logout(request):
+    logout(request)
+    return render(request, 'main.html')
+
 def register(request):
     if request.method == "POST":
-        form = CustomUserForm(data=request.POST or None)
+        form = CustomUserForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            form.save()
+
+            user_id = form.cleaned_data['username']
+            user_pwd = form.cleaned_data['password']
+            user = authenticate(request, username=user_id, password=user_pwd)
             login(request, user)
-            return redirect('main')
+            return redirect('market:main')
     else:
         form = CustomUserForm()
     return render(request, 'registration/register.html', {"form": form})
