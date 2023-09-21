7377470d05d9e9070d04fdebb768a25cf38e9600
Leeeyounghwan <lee.yhwan226@gmail.com>
2023-09-21 15:10:10 +0900

    feat: 회원가입 및 로그인 폼 구현
    Resolves: #6

--- danggeun/market/views.py
+++ danggeun/market/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from .forms import CustomAuthForm, CustomUserForm
 
 def main(request):
     return render(request, 'main.html')
@@ -10,12 +15,32 @@ def chat(request):
     return render(request, 'chat.html')
 
 def login(request):
-    return render(request, 'registration/login.html')
+    if request.user.is_authenticated:
+        return redirect('market:main')
+    else:
+        form = CustomAuthForm(data=request.POST or None)
+        if request.method == "POST":
+            if form.is_valid():
+                user_id = form.cleaned_data['username']
+                user_pwd = form.cleaned_data['userpassword_pwd']
+                user = authenticate(request, username=user_id, password=user_pwd)
+                if user is not None:
+                    login(request, user)
+                    return redirect('market:main')
+        return render(request, 'registration/login.html', {"form": form})
 
 # def logout(request):
 #     return render(request, 'registration/logout.html')
 def register(request):
-    return render(request, 'registration/register.html')
+    if request.method == "POST":
+        form = CustomUserForm(data=request.POST or None)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('main')
+    else:
+        form = CustomUserForm()
+    return render(request, 'registration/register.html', {"form": form})
 
 def trade(request):
     return render(request, 'trade.html')
