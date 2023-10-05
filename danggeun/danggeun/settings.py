da86f476507d30ffbceb679ab3bd36c8126780f7
LeeYeonghwan <129285372+Leeeyounghwan@users.noreply.github.com>
2023-10-05 10:48:12 +0900

    feat: css 수정 (#39)
    * feat: 찜하기 기능 추가
    
    html 수정 및  찜하기 기능 구현 완료
    
    Resolves:#32
    
    * feat: 찜하기 기능 추가
    
    마이페이지 관심목록에 찜한 상품들 표시
    
    Resolves: #32
    
    * feat: css 수정
    
    전체 페이지 스크롤 바 삭제
    
    ---------
    
    Co-authored-by: OGUbanana <114931412+OGUbanana@users.noreply.github.com>

--- danggeun/danggeun/settings.py
+++ danggeun/danggeun/settings.py
@@ -181,13 +181,13 @@ AUTH_USER_MODEL = 'auth.User'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-KR'
 
 TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
