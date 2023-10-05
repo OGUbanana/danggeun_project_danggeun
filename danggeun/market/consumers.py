2dd93b3cdddc853dd2e7d77073bab9b8da81c3e9
OGUbanana <bbeunjin@gmail.com>
2023-10-05 15:03:39 +0900

    feat: 시간 추가

--- danggeun/market/consumers.py
+++ danggeun/market/consumers.py
@@ -38,7 +38,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         username = text_data_json["username"]
 
         # Save the message to the database
-        await self.save_message(username, message)
+        
+        saved_message = await self.save_message(username, message)
+        timestamp = saved_message.timestamp.strftime('%Y-%m-%d %H:%M:%S')
 
         # Send message to room group
         await self.channel_layer.group_send(
@@ -46,16 +48,16 @@ class ChatConsumer(AsyncWebsocketConsumer):
             {
                 "type": "chat_message",
                 "message": message,
-                "username": username
+                "username": username,
+                "timestamp": timestamp
             }
         )
 
-    @database_sync_to_async # 데코레이터를 사용하여 메서드를 비동기적으로 실행
+    @database_sync_to_async
     def save_message(self, username, message):
-        user = User.objects.get(username=username) # 사용자를 데이터베이스에서 가져옴
-        chat_room = ChatRoom.objects.get(room_number=self.room_name) #채팅방을 데이터베이스에서 가져옴
-
-        ChatMessage.objects.create( # 새로운 메시지 객체를 생성하고 데이터베이스에 저장
+        user = User.objects.get(username=username)
+        chat_room = ChatRoom.objects.get(room_number=self.room_name)
+        return ChatMessage.objects.create(
             chatroom=chat_room,
             author=user,
             content=message
@@ -64,17 +66,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event["message"]
         username = event["username"]
+        timestamp = event["timestamp"]
 
         await self.send(text_data=json.dumps({
             "message": message,
-            "username": username
+            "username": username,
+            "timestamp": timestamp
         }))
 
     @database_sync_to_async
     def load_previous_messages(self):
         chat_room = ChatRoom.objects.get(room_number=self.room_name)
         messages = chat_room.messages.all()
-        return [(message.content, message.author.username) for message in messages]
+        return [(message.content, message.author.username, message.timestamp.strftime('%Y-%m-%d %H:%M:%S')) for message in messages]
 
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
@@ -88,8 +92,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
         previous_messages = await self.load_previous_messages()
-        for message_content, username in previous_messages:
+        for message_content, username, timestamp in previous_messages:
             await self.send(text_data=json.dumps({
                 "message": message_content,
-                "username": username
+                "username": username,
+                "timestamp": timestamp
             }))
